6066b411387b1daa8aeb6899de58209c7fdfdae5
FC61813 <fc61813@alunos.fc.ul.pt>
2026-08-06 20:09:28 +0100

    inicio

---
+++ projectsData.js
@@ -0,0 +1,11 @@
+// js/projectsData.js
+const projetos = [
+    {
+        titulo: "Catálogo Digital + Gestor de Encomendas - Pastelaria Torrita",
+        descricao: "Design e desenvolvimento de um website responsivo para apresentação de produtos e catálogo.",
+        tecnologias: ["HTML", "CSS", "JavaScript"],
+        imagem: "assets/img/projects/torrita-mockup.png",
+        linkRepo: "https://github.com/...",
+        linkLive: "https://rafafigas12.github.io/Torrita/"
+    }
+];
